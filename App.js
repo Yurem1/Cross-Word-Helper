e494cdef9709aeffae2514e5391b004755696860
Yurem1 <yuremtovar1@gmail.com>
2023-12-29 14:25:20 -0600

    Update App.js

--- App.js
+++ App.js
@@ -2,15 +2,15 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 import { Title } from './components/Title';
-import { UploadButton } from './components/Button';
-import { DisplayLogo } from './components/Screen';
+import UploadButton from './components/Button';
+import DisplayLogo from './components/Screen';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Title></Title>
       <DisplayLogo></DisplayLogo>
-      <UploadButton scale='.95' opacity={0.9}></UploadButton>
+      <UploadButton></UploadButton>
       <StatusBar style="auto" />
     </View>
   );
