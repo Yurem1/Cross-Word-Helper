4dad65d927dbf933534eccbaa384555de4dfb076
Yurem1 <yuremtovar1@gmail.com>
2023-12-29 14:25:54 -0600

    Update Title.js

--- components/Title.js
+++ components/Title.js
@@ -3,27 +3,39 @@ import { StyleSheet, Text, View } from 'react-native';
 import { useFonts } from 'expo-font';
 
 export function Title() {
-    const [isLoaded, error] = useFonts({
+    [isLoaded] = useFonts({
+        'Rubik-Doodle-Shadow-Regular': require('../assets/fonts/Rubik-Doodle-Shadow-Regular.ttf'),
         'RubikScribble-Regular': require('../assets/fonts/RubikScribble-Regular.ttf')
     });
 
     if(!isLoaded) {
         return <Text>Loading Text...</Text>
-    } else if (error != null) {
-        return <Text>{error}</Text>
     }
 
     return (
-        <View>
-            <Text style={styles.text}>Cross Word Helper</Text>
+        <View style={styles.container}>
+            <Text style={styles.title}>Cross Word Helper</Text>
+            <Text style={styles.description}>Press the button and upload your image for scanning</Text>
         </View>
     );
 }
 
 const styles = StyleSheet.create({
-    text: {
-        color: 'red',
-        fontSize: 20,
-        fontFamily: 'RubikScribble-Regular'
+    container: {
+        marginTop: 60,
+        flex: 1
+    },
+    title: {
+        color: '#21005D',
+        marginBottom: 10,
+        textAlign: 'center',
+        fontSize: 40,
+        fontFamily: 'Rubik-Doodle-Shadow-Regular'
+    },
+    description: {
+        color: '#381E72',
+        textAlign: 'center',
+        fontSize: 15,
+        fontWeight: '300'
     }
 });
