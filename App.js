b3ca1499e2499457f1e7be0d92fc7e832077ed1e
Yurem1 <yuremtovar1@gmail.com>
2023-12-30 02:36:56 -0600

    Update App.js

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-import { Title } from './components/Title';
+import Title from './components/Title';
 import UploadButton from './components/Button';
 import DisplayLogo from './components/Screen';
 
