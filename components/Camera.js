e0415612b615fb3e930bed5455a55f806f22e78f
Yurem1 <yuremtovar1@gmail.com>
2024-01-01 21:59:21 -0600

    Remove all JS components

--- components/Camera.js
+++
@@ -1,7 +0,0 @@
-import { Camera, CameraType } from 'expo-camera';
-import { useState } from 'react';
-
-export default function Camera() {
-  const [type, setType] = useState(CameraType.back);
-  setType('e');
-}
