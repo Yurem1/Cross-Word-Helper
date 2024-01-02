fbbc15ee35023a224d6ec62c9d720fe13826368d
Yurem1 <yuremtovar1@gmail.com>
2024-01-01 21:58:43 -0600

    Add navigator screens

--- App.js
+++ App.js
@@ -1,27 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
+import * as React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
-import Title from './components/Title';
-import UploadButton from './components/Button';
-import DisplayLogo from './components/Screen';
+import HomeScreen from './screens/homescreen/HomeScreen'
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Title></Title>
-      <DisplayLogo></DisplayLogo>
-      <UploadButton></UploadButton>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+    <NavigationContainer>
+        <Stack.Navigator>
+            <Stack.Screen 
+            name='Home Screen'
+            component={HomeScreen}
+            options={{
+              headerStyle: {
+                backgroundColor: '#7D5260',
+              },
+              headerTintColor: '#fff',
+              headerTitleStyle: {
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    flexDirection: 'column',
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+                fontWeight: 'bold',
+              }
+            }}>
+            </Stack.Screen>
+        </Stack.Navigator>
+    </NavigationContainer>
+);
+}
