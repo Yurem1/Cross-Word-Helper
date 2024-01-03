8e0073324d80886a6bfbdb4350396c9ade4952cd
Yurem1 <yuremtovar1@gmail.com>
2024-01-03 16:58:44 -0600

    Update Information.tsx, add more text, etc

--- screens/homescreen/components/Information.tsx
+++ screens/homescreen/components/Information.tsx
@@ -1,24 +1,13 @@
-import React, { Dispatch, SetStateAction, useState } from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, View, Text, TouchableOpacity } from 'react-native';
+import { FontAwesome5, Feather } from '@expo/vector-icons';
+import FetchIcon from '../../../main_components/FetchIcon';
 
-import InformationText from '../text/screen.json'
+import InformationText from '../../../assets/text/screen.json';
 
 interface ToggleInformation {
   onPress: boolean
 }
-
-function MoreInformation({onPress}: ToggleInformation): React.JSX.Element {
-  return onPress && (
-    <View style={styles.moreInformation}>
-      <Text style={styles.text}>
-      {'\u2192'} {InformationText.homescreen.info.more_info.title}
-      </Text>
-      <Text>
-        Another day, another victory for the og..
-      </Text>
-    </View>
-  );
-};
 
 export default function Information(): React.JSX.Element {
   const [
@@ -44,13 +33,62 @@ export default function Information(): React.JSX.Element {
       onPressOut={(): void => setPressFlags(false)}
       onPress={(): void => infoFlags ? setInfoFlags(false) : setInfoFlags(true)}>
         <View style={dynamicStyle}>
-          <Text style={styles.text}>
-            {InformationText.homescreen.info.title}
+          <Text style={styles.text_title}>
+            {InformationText.info.title} <FontAwesome5 name="info-circle" size={15} color="black"/>
           </Text>
         </View>
       </TouchableOpacity>
       <MoreInformation onPress={infoFlags}>
       </MoreInformation>
+    </View>
+  );
+};
+
+function MoreInformation({onPress}: ToggleInformation): React.JSX.Element {
+  const [
+    isPressed,
+    setPressed
+  ]: any = useState<boolean>(false);
+
+  const dynamicStyle: any = {
+    ...styles.moreInformation,
+    transform: isPressed ? 'scale(.95)' : 'scale(1)'
+  }
+
+  return onPress && (
+    <View style={{...styles.container, top:10}}>
+      <TouchableOpacity
+      activeOpacity={.9}
+      onPressIn={(): void => setPressed(true)}
+      onPressOut={(): void => setPressed(false)}
+      >
+        <View style={dynamicStyle}>
+          <Text style={styles.text_title}>
+            <FetchIcon 
+            Type={FontAwesome5}
+            name="camera" 
+            /> {InformationText.info.steps.step_one}
+          </Text>
+          <Text style={styles.text_normal}>
+            <FetchIcon 
+            Type={FontAwesome5}
+            name="image" 
+            /> {InformationText.info.steps.step_two}
+          </Text>
+          <Text style={styles.text_normal}>
+            <FetchIcon
+            Type={FontAwesome5} 
+            name="tools" 
+              /> {InformationText.info.steps.step_three}
+          </Text>
+          <Text style={styles.text_normal}>
+            <FetchIcon
+            Type={FontAwesome5}
+            name="grin-beam" 
+              /> {InformationText.info.steps.step_four}
+          </Text>
+        </View>
+      </TouchableOpacity>
     </View>
   );
 };
@@ -61,24 +99,40 @@ const styles: any = StyleSheet.create({
     position: 'relative',
     top: 20,
   },
+  icon: {
+    flexDirection: 'row',
+  },
   information: {
     padding: 10,
     borderRadius: 30,
-    shadowColor: '#000',
-    shadowOffset: { width: 0, height: 1 },
+    shadowOffset: {
+      width: 0,
+      height: 1 
+    },
     shadowOpacity: 0.8,
     shadowRadius: 1,
+    shadowColor: 'black',
     backgroundColor: '#FFFFFF',
   },
   moreInformation: {
     padding: 10,
-    marginTop: 10,
-    borderRadius: 10,
-    borderWidth: 1,
-    borderTopColor: 'black',
+    borderRadius: 25,
+    shadowOffset: {
+      width: 0,
+      height: 1 
+    },
+    shadowOpacity: 0.8,
+    shadowRadius: 1,
+    shadowColor: 'black',
     backgroundColor: '#FFFFFF',
   },
-  text: {
+  text_title: {
     textAlign: 'center',
+    fontWeight: '500',
+  },
+  text_normal: {
+    textAlign: 'center',
+    fontWeight: '500',
+    marginTop: 5
   }
 });
