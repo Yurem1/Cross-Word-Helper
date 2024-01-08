92c3b48055082ae05228e5c233d6dd46eacbbfc9
Yurem1 <yuremtovar1@gmail.com>
2024-01-07 21:33:03 -0600

    Add Camera.tsx (camera screen component)

---
+++ screens/camera/Camera.tsx
@@ -0,0 +1,10 @@
+import React, { useState } from 'react';
+import { StyleSheet, View, Image, Text } from 'react-native';
+
+export default function Test(): any {
+    return (
+        <Text>
+            Hi
+        </Text>
+    )
+}
