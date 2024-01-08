f3fbbb612681720d9af282a7398233655681d1ad
Yurem1 <yuremtovar1@gmail.com>
2024-01-07 21:32:17 -0600

    Add a second screen (camera)

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
 import HomeScreen from './screens/homescreen/HomeScreen'
+import Camera from './screens/camera/Camera';
 
 const Stack = createNativeStackNavigator();
 
@@ -12,8 +13,22 @@ export default function App() {
     <NavigationContainer>
         <Stack.Navigator>
             <Stack.Screen 
-            name='Home Screen'
+            name='Home'
             component={HomeScreen}
+            options={{
+              headerStyle: {
+                backgroundColor: '#7D5260',
+              },
+              headerTintColor: '#fff',
+              headerTitleStyle: {
+
+                fontWeight: 'bold',
+              }
+            }}>
+            </Stack.Screen>
+            <Stack.Screen
+            name='Camera'
+            component={Camera}
             options={{
               headerStyle: {
                 backgroundColor: '#7D5260',
