1d559077178321a16bfd4af49495d361d214b75a
Yurem1 <yuremtovar1@gmail.com>
2024-01-08 19:03:14 -0600

    Add comments

--- main_components/ButtonPress.tsx
+++ main_components/ButtonPress.tsx
@@ -1,6 +1,9 @@
 import React, { ReactNode } from 'react';
 import { TouchableOpacity } from 'react-native';
 
+/**
+ * @summary Blueprint for `TouchableOpacity` components
+ */
 interface ToggleButton {
     opacity?: number;
     flags: (flags: boolean) => void;
@@ -8,6 +11,11 @@ interface ToggleButton {
     children: ReactNode;
 }
 
+/**
+ * @summary A more refined and less verbose <TouchableOpacity> component
+ * @param param0 Props inherited from `TouchableOpacity`, such as opacity, press, etc
+ * @returns A pressable button with opacity
+ */
 export default function ButtonPress({opacity = 0.8, flags, press, children}: ToggleButton): React.JSX.Element {
     return (
         <TouchableOpacity activeOpacity={opacity} onPress={press} onPressIn={() => flags(true)} onPressOut={() => flags(false)}>
