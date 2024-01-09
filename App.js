8dddb7d828e11a3f1a7f6821675140810045efaf
Yurem1 <yuremtovar1@gmail.com>
2024-01-08 19:01:31 -0600

    Remove unused imports

--- App.js
+++ App.js
@@ -1,5 +1,4 @@
 import * as React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
@@ -42,5 +41,5 @@ export default function App() {
             </Stack.Screen>
         </Stack.Navigator>
     </NavigationContainer>
-);
+    );
 }
